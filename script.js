8589f3fd706fac96c48d01a323916d073d225947
Ruslan Moskvitin <moskvitin.ra@phystech.edu>
2021-05-22 09:48:29 +0300

    feat: до конца реализовал JS-код

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ for(let i = 0; i < elementList.length; i += 1) {
 }
 
 //Здесь я реализовал кнопку Like.//
+
+let popup = page.querySelector('.popup');
+let popupContainer = popup.querySelector('.popup__container');
+let popupEditForm = popupContainer.querySelector('.popup__edit-form');
+let popupCloseButton = popupEditForm.querySelector('.popup__close-button');
+let profile = content.querySelector('.profile');
+let popupOpenButton = profile.querySelector('.profile__edit-button');
+let profileFullName = profile.querySelector('.profile__full-name'); //h1 tag
+let profileRole = profile.querySelector('.profile__role'); //p tag
+let popupPersonInfoInputs = popupEditForm.querySelectorAll('.popup__input');
+
+function takeInputsFromPage() {
+  popupPersonInfoInputs[0].value = profileFullName.textContent;
+  popupPersonInfoInputs[1].value = profileRole.textContent;
+}
+
+popupCloseButton.addEventListener('click', function() {
+  popup.classList.remove('popup_opened');
+  takeInputsFromPage();
+})
+
+popupOpenButton.addEventListener('click', function() {
+  popup.classList.add('popup_opened');
+  takeInputsFromPage();
+})
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileFullName.textContent = popupPersonInfoInputs[0].value;
+  profileRole.textContent = popupPersonInfoInputs[1].value;
+  popup.classList.remove('popup_opened');
+}
+
+popupEditForm.addEventListener('submit', formSubmitHandler);
