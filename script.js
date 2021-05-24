bb42164af9db4ea1c10e4aebe27a77140c26910c
Ruslan Moskvitin <moskvitin.ra@phystech.edu>
2021-05-24 10:52:25 +0300

    bugfix: исправил высоту полей ввода

--- script.js
+++ script.js
@@ -2,16 +2,6 @@ let page = document.querySelector('.page');
 let content = page.querySelector('.content');
 let elements = content.querySelector('.elements');
 let elementList = elements.querySelectorAll('.element');
-
-for(let i = 0; i < elementList.length; i += 1) {
-  let likeButton = elementList[i].querySelector('.element__like-button');
-  likeButton.addEventListener('click', function() {
-    likeButton.classList.toggle('element__like-button_active');
-  })
-}
-
-//Здесь я реализовал кнопку Like.//
-
 let popup = page.querySelector('.popup');
 let popupContainer = popup.querySelector('.popup__container');
 let popupEditForm = popupContainer.querySelector('.popup__edit-form');
