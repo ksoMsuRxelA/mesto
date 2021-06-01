5e494b83247b1b3621dfb8b2a5f40d2a4e180eae
Ruslan Moskvitin <moskvitin.ra@phystech.edu>
2021-06-01 13:42:35 +0300

    feat: реализовал открытие и закрытие попап-ов

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,43 @@ let page = document.querySelector('.page');
 let content = page.querySelector('.content');
 let elements = content.querySelector('.elements');
 let elementList = elements.querySelectorAll('.element');
-let popup = page.querySelector('.popup');
-let popupContainer = popup.querySelector('.popup__container');
-let popupEditForm = popupContainer.querySelector('.popup__edit-form');
-let popupCloseButton = popupEditForm.querySelector('.popup__close-button');
+
+let popup = page.querySelectorAll('.popup');
+let popupContainerPerson = popup[0].querySelector('.popup__container');
+let popupEditFormPerson = popupContainerPerson.querySelector('.popup__edit-form');
+let popupCloseButtonPerson = popupEditFormPerson.querySelector('.popup__close-button');
+
 let profile = content.querySelector('.profile');
-let popupOpenButton = profile.querySelector('.profile__edit-button');
+let popupOpenButtonPerson = profile.querySelector('.profile__edit-button');
 let profileFullName = profile.querySelector('.profile__full-name'); //h1 tag
 let profileRole = profile.querySelector('.profile__role'); //p tag
-let popupPersonInfoInputs = popupEditForm.querySelectorAll('.popup__input');
+let popupPersonInfoInputs = popupEditFormPerson.querySelectorAll('.popup__input');
 
 function takeInputsFromPage() {
   popupPersonInfoInputs[0].value = profileFullName.textContent;
   popupPersonInfoInputs[1].value = profileRole.textContent;
 }
 
-popupCloseButton.addEventListener('click', function() {
-  popup.classList.remove('popup_opened');
-})
+function popupOpenClass(popupIndex) {
+  return function() {
+    popup[popupIndex].classList.add('popup_opened');
+  }
+}
 
-popupOpenButton.addEventListener('click', function() {
-  popup.classList.add('popup_opened');
-  takeInputsFromPage();
-})
+function popupCloseClass(popupIndex) {
+  return function() {
+    popup[popupIndex].classList.remove('popup_opened');
+  }
+}
+
+popupOpenButtonPerson.addEventListener('click', popupOpenClass(0));
+popupCloseButtonPerson.addEventListener('click', popupCloseClass(0));
+
+const popupOpenButtonAdd = profile.querySelector('.profile__add-button');
+const popupCloseButtonAdd = popup[1].querySelector('.popup__close-button');
+
+popupOpenButtonAdd.addEventListener('click', popupOpenClass(1));
+popupCloseButtonAdd.addEventListener('click', popupCloseClass(1));
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -33,7 +47,7 @@ function formSubmitHandler (evt) {
   popup.classList.remove('popup_opened');
 }
 
-popupEditForm.addEventListener('submit', formSubmitHandler);
+popupEditFormPerson.addEventListener('submit', formSubmitHandler);
 
 
 //Здесь начинается код нового спринта. 
@@ -75,4 +89,5 @@ Array.from(elements.children).forEach((item, index) => {
   item.querySelector('.element__image').src = card.link;
   item.querySelector('.element__title').textContent = card.name;
 });
+
 
