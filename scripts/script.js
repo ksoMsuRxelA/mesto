f4755d4e5885eb630c67088aa7641daba2170731
Ruslan Moskvitin <moskvitin.ra@phystech.edu>
2021-06-01 11:01:04 +0300

    feat: реализовал установки карточек по умолчанию

--- scripts/script.js
+++ scripts/script.js
@@ -34,3 +34,45 @@ function formSubmitHandler (evt) {
 }
 
 popupEditForm.addEventListener('submit', formSubmitHandler);
+
+
+//Здесь начинается код нового спринта. 
+
+/*Шесть карточек из коробки*/
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+/*Попробуем воспльзоваться новыми знаниями, а именно методом .children
+и методом .from глобального объекта Array*/
+
+Array.from(elements.children).forEach((item, index) => {
+  const card = initialCards[index];
+  item.querySelector('.element__image').src = card.link;
+  item.querySelector('.element__title').textContent = card.name;
+});
+
