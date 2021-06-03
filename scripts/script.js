5d969d8509d5c2eb6aa8b7050614ba5c3a54fdc1
Ruslan Moskvitin <moskvitin.ra@phystech.edu>
2021-06-03 15:25:04 +0300

    feat: реализовал открытие попапа с картинкой

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,11 @@ const popupEditFormAdd = popupAdd.querySelector('.popup__edit-form');
 const popupCloseButtonAdd = popupAdd.querySelector('.popup__close-button');
 const popupInfoInputsAdd = popupAdd.querySelectorAll('.popup__input');
 
+const popupImage = page.querySelector('.popup_type_image');
+const popupImg = popupImage.querySelector('.popup__image');
+const popupCaption = popupImage.querySelector('.popup__image-caption');
+const popupCloseButtonImage = popupImage.querySelector('.popup__close-button');
+
 const profile = content.querySelector('.profile');
 const popupOpenButtonEdit = profile.querySelector('.profile__edit-button');
 const popupOpenButtonAdd = profile.querySelector('.profile__add-button');
@@ -40,6 +45,8 @@ popupCloseButtonEdit.addEventListener('click', popupToggle(popupEdit));
 
 popupOpenButtonAdd.addEventListener('click', popupToggle(popupAdd));
 popupCloseButtonAdd.addEventListener('click', popupToggle(popupAdd));
+
+popupCloseButtonImage.addEventListener('click', popupToggle(popupImage));
 
 function formSubmitHandlerEdit (evt) {
   evt.preventDefault();
@@ -92,10 +99,15 @@ function makeCard(title, link) {
     const delParent = event.target.closest('.element');
     delParent.remove();
   });
-
+  newCard.querySelector('.element__image').addEventListener('click', function(event) {
+    popupCaption.textContent = event.target.nextElementSibling.textContent;
+    popupImg.src = event.target.src;
+    popupImg.alt = event.target.nextElementSibling.textContent;
+    popupImage.classList.add('popup_opened');
+  });
   newCard.querySelector('.element__title').textContent = title;
   newCard.querySelector('.element__image').src = link;
-
+  newCard.querySelector('.element__image').alt = title;
   elements.prepend(newCard);
 }
 
