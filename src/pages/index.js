0190d9f52e86d89b9da2ccfd17e2a055e7c37c15
Ruslan Moskvitin <moskvitin.ra@phystech.edu>
2021-07-17 01:11:56 +0300

    feat: настроил сборку с помощью webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import UserInfo from '../components/UserInfo.js';
